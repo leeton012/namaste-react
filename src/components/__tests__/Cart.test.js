038dbd83b432b96af2059afe150d3da243e385d0
leeton012 <princemondal012@gmail.com>
2023-11-20 15:55:33 +0530

    Test cases

--- src/components/__tests__/Cart.test.js
+++ src/components/__tests__/Cart.test.js
@@ -3,10 +3,23 @@ import RestaurantMenu from '../RestaurantMenu';
 import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom';
 import MOCK_DATA from '../mocks/mockRestaurantItems.json';
+// import MOCK_RES_DATA from '../mocks/mockRestaurantList.json';
 import { Provider } from 'react-redux';
 import appStore from '../../utils/appStore';
 import Header from '../Header';
 import { BrowserRouter } from 'react-router-dom';
+import Cart from '../Cart';
+import '@testing-library/jest-dom';
+// import MOCK_RES_DATA from '../mocks/mockRestaurantList.json';
+import Body from '../Body';
+
+// global.fetch = jest.fn(() => {
+//   return Promise.resolve({
+//     json: () => {
+//       return Promise.resolve(MOCK_RES_DATA);
+//     },
+//   });
+// });
 
 global.fetch = jest.fn(() => {
   return Promise.resolve({
@@ -20,8 +33,10 @@ it('SHould add the product to the cart in Header', async () => {
   await act(async () =>
     render(
       <BrowserRouter>
+        {/* <Body /> */}
         <Provider store={appStore}>
           <Header />
+          <Cart />
           <RestaurantMenu />
         </Provider>
       </BrowserRouter>
@@ -46,4 +61,16 @@ it('SHould add the product to the cart in Header', async () => {
   fireEvent.click(addBtns[1]);
 
   expect(screen.getByText('Cart(2)')).toBeInTheDocument();
+
+  expect(screen.getAllByTestId('foodItems').length).toBe(7);
+
+  fireEvent.click(screen.getByRole('button', { name: 'Clear Cart' }));
+
+  expect(screen.getByText('Your Cart is empty. Please do more shopping....')).toBeInTheDocument();
+
+  expect(screen.getByRole('button', { name: 'Continue Shopping' })).toBeInTheDocument();
+
+  fireEvent.click(screen.getByRole('button', { name: 'Continue Shopping' }));
+
+  //   expect(screen.getAllByTestId('resCard').length).toBe(20);
 });
